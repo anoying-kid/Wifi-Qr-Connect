4e02073ea80e2432400ffc89510f38b7c9e4631d
anoying-kid <yugrana853@gmail.com>
2026-06-14 15:59:00 +0530

    feat: bootstrap native Swift app structure

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "WifiQrConnect",
+    platforms: [
+        .macOS(.v14),
+    ],
+    targets: [
+        .executableTarget(
+            name: "WifiQrConnect",
+            path: "Sources/WifiQrConnect",
+            resources: [
+                .process("Resources"),
+            ])
+    ]
+)
---
+++ Sources/WifiQrConnect/main.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct WifiQrConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello from WifiQrConnect")
+                .padding()
+        }
+    }
+}
+
+WifiQrConnectApp.main()
